dceb7595713d98d0787f6ae8d6ab6ee6e7579e68
Jonavin <Jonavin@users.noreply.github.com>
2014-12-08 10:34:12 +0800

    Update util.js

--- util.js
+++ util.js
@@ -12,3 +12,18 @@
 
       return (_isNeg?"10":"0")+defS.substring(0,_dif).replace(_reg,"$1"+_ns.replace("-",""));
     }
+    
+     /**
+    **转换为10进制，目前只考虑输入为2进制，因为默认定义的数字为十进制所以输入参数为2进制数字的字符串表示
+    **/
+    function toDecimalString(num){
+       var _n = num.length===32,//2进制字符串的长度是否等于32位
+           _isNeg = false;//是否是负数
+       if(_n){
+         _isNeg = num.charAt(0) == 1;
+         num = num.substring(1);//去除符号位
+       }
+       var _rg = /(0*)1/,
+           num = num.replace(_rg,""+1);
+      return (_isNeg?"-":"")+parseInt(num,2)
+    } 
