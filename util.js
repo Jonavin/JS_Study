67edceb39d1676762dc02db5e890e5c05f42dda9
Jonavin <Jonavin@users.noreply.github.com>
2014-12-08 11:18:52 +0800

    Update util.js

--- util.js
+++ util.js
@@ -1,5 +1,10 @@
-      /**
-    **转换为2进制，目前只考虑输入为十进制
+ /**
+    JS中如果整数内存大于32位时, JS会自动屏蔽掉32位以后字节
+    **/
+        
+    
+    /**
+    **转换为2进制，目前只考虑输入为十进制,只针对内存在32位之内的数值（2147483647）
     **/
     var defS = "0000000000000000000000000000000";//31个零
     function toBinaryString(num){
@@ -9,11 +14,9 @@
           _rs = "(0{"+_dif+"})",
           _reg = new RegExp(_rs),
           _isNeg = _ns.charCodeAt(0) === 45;//是否是负数
-
       return (_isNeg?"10":"0")+defS.substring(0,_dif).replace(_reg,"$1"+_ns.replace("-",""));
     }
-    
-     /**
+    /**
     **转换为10进制，目前只考虑输入为2进制，因为默认定义的数字为十进制所以输入参数为2进制数字的字符串表示
     **/
     function toDecimalString(num){
@@ -26,4 +29,4 @@
        var _rg = /(0*)1/,
            num = num.replace(_rg,""+1);
       return (_isNeg?"-":"")+parseInt(num,2)
-    } 
+    }
