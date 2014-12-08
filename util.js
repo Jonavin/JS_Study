51aba48f3e8e0949d3cefa478df7b5d4cfde1500
Jonavin <Jonavin@users.noreply.github.com>
2014-12-08 10:27:02 +0800

    Update util.js

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
-    /**
-    **转换为2进制
+      /**
+    **转换为2进制，目前只考虑输入为十进制
     **/
     var defS = "0000000000000000000000000000000";//31个零
     function toBinaryString(num){
@@ -10,5 +10,5 @@
           _reg = new RegExp(_rs),
           _isNeg = _ns.charCodeAt(0) === 45;//是否是负数
 
-      return (_isNeg?"1":"0")+defS.substring(0,_dif).replace(_reg,"$1"+_ns.replace("-",""));
+      return (_isNeg?"10":"0")+defS.substring(0,_dif).replace(_reg,"$1"+_ns.replace("-",""));
     }
