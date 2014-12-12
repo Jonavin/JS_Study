1467c8295a87e99570f10fc8c8879652b3a8e21d
Jonavin <Jonavin@users.noreply.github.com>
2014-12-12 11:38:56 +0800

    Update util.js

--- util.js
+++ util.js
@@ -112,3 +112,32 @@
       var s = location.search,rg = new RegExp(key+"=(\\w)&*"),r = rg.test(s);
       return RegExp["$+"];
     }
+  //下边方法的数据
+   var abd = [{
+        text: "123",
+        children: [{
+            text: "123_1",
+            children: [{
+                text: "123_1_1",
+                children: [{
+                    text: "123_1_1_1",
+                    children: []
+                },
+                {
+                    text: "123_1_1_2",
+                    children: []
+                }]
+            },
+            {
+                text: "123_1_2",
+                children: []
+            }]
+        }]
+    }];
+    
+    //获得最深而且含有子节点的节点
+    function getDeepest(){
+      var s = JSON.stringify(abd).trim(), rg = /\[\{("text":("\w+"*?))/g;
+      alert(s.match(rg));
+    }
+    getDeepest();
