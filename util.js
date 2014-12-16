4b2cc2d4300191459d000113155059cefa586779
Jonavin <Jonavin@users.noreply.github.com>
2014-12-16 18:11:32 +0800

    Update util.js

--- util.js
+++ util.js
@@ -165,3 +165,59 @@
       })();
       return ret;
     }
+    
+    
+    //未知字符
+    function fixedCharCodeAt (str, idx) {
+          idx = idx || 0;
+          var code = str.charCodeAt(idx);
+          var hi, low;
+          if (0xD800 <= code && code <= 0xDBFF) {
+              hi = code;
+              low = str.charCodeAt(idx+1);
+              if (isNaN(low)) {
+                  throw 'High surrogate not followed by low surrogate in fixedCharCodeAt()';
+              }
+              return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+          }
+          if (0xDC00 <= code && code <= 0xDFFF) { 
+              return false;
+          }
+          return code;
+      }
+      
+       alert(fixedCharCodeAt("大"));
+    
+    
+    
+    //已知字符
+    function knownCharCodeAt (str, idx) {
+        str += '';
+        var code,
+            end = str.length;
+
+        var surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+        while ((surrogatePairs.exec(str)) != null) {
+            var li = surrogatePairs.lastIndex;
+            if (li - 2 < idx) {
+                idx++;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (idx >= end || idx < 0) {
+            return NaN;
+        }
+
+        code = str.charCodeAt(idx);
+      
+        var hi, low;
+        if (0xD800 <= code && code <= 0xDBFF) {
+            hi = code;
+            low = str.charCodeAt(idx+1);
+            return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+        }
+        return code;
+    }
