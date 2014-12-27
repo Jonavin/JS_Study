11bd884d39a6b70e47db7a6db8ef89b3562055e4
Jonavin <Jonavin@users.noreply.github.com>
2014-12-27 19:56:57 +0800

    Update utils.js

--- util/utils.js
+++ util/utils.js
@@ -1,3 +1,4 @@
 ;(function (){
-
+  //定义事件
+  var events = "click dblclick mousedown mousemove mouseout mouseover mouseup touchstart touchmove touchend touchcancel"["split"](" ");
 })(window,document)
