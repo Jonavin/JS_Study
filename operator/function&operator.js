921e39d7719432fd186d6fc90a38a5d375f69c22
Jonavin <Jonavin@users.noreply.github.com>
2015-01-08 10:08:49 +0800

    Create function&operator.js

---
+++ operator/function&operator.js
@@ -0,0 +1,15 @@
+    function f (){
+      alert(1);
+    };
+    console.log(f);
+    console.log(+f);
+    console.log(-f);
+    console.log(void f);
+    console.log(~f);
+    console.log(++f);
+    console.log(--f);
+    console.log(delete f);
+    console.log(f);
+    console.log(f++);
+    console.log(f>>>1);
+    console.log(f);
